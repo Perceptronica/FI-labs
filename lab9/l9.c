95a8e0ed2e35527300b8542cbf50ad87c1c1ac1f
Anton Kabanov <klvlwxxw@ya.ru>
2022-11-16 16:36:17 +0300

    новый файл:    lab1/debugger.tex 	новый файл:    lab1/lab1.aux 	новый файл:    lab1/lab1.log 	изменено:      lab1/lab1.pdf 	новый файл:    lab1/lab1.synctex.gz 	новый файл:    lab1/lab1.tex 	новый файл:    lab1/lab1_old.pdf 	новый файл:    lab1/protocol.tex 	новый файл:    lab1/testprotocol.aux 	новый файл:    lab1/testprotocol.log 	новый файл:    lab1/testprotocol.pdf 	новый файл:    lab1/testprotocol.synctex.gz 	новый файл:    lab1/testprotocol.tex 	новый файл:    lab9/.ipynb_checkpoints/Untitled-checkpoint.ipynb 	новый файл:    lab9/.ipynb_checkpoints/visuals-checkpoint.ipynb 	новый файл:    lab9/Untitled.ipynb 	новый файл:    lab9/l9.c 	новый файл:    lab9/visuals.ipynb 	новый файл:    tex/content.tex 	новый файл:    tex/preamble/preamble.log 	новый файл:    tex/preamble/preamble.tex 	новый файл:    tex/template.aux 	новый файл:    tex/template.log 	новый файл:    tex/template.pdf 	новый файл:    tex/template.synctex.gz 	новый файл:    tex/template.tex

---
+++ lab9/l9.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int x, int y) {
+	if (x <= y) {
+		return y;
+	} else {
+		return x;
+	}
+}
+
+int min(int x, int y) {
+	if (x <= y) {
+		return x;
+	} else {
+		return y;
+	}
+}
+
+int modulo(int x, int y) {
+	int k = 0;
+	if (x < 0) {
+		if (abs(x) % abs(y) == 0) {
+			k = 0;
+		} else {
+			k = 1;
+		}
+	}
+	return x - (x / y - k) * y;
+}
+
+int main() {
+	const int i0 = 11;
+	const int j0 = 13;
+	const int l0 = 10;
+	//printf("%d\n", modulo(23, 10));
+	int i_old = i0, j_old = j0, l_old = l0;
+	int i_next, j_next, l_next;
+	int k = 0;
+	int flag = 0;
+	while (1) {
+		i_next = abs(k - 15) - min(i_old / 3, modulo((j_old + l_old), 10)) - 20;
+		j_next = -(j_old + k) / 5 + abs(modulo(i_old * l_old, 8));
+		l_next = max(modulo(i_old + j_old, 15), modulo(l_old + k, 14));
+		i_old = i_next;
+		j_old = j_next;
+		l_old = l_next;
+		k++;
+		if ((i_old > 5 && i_old < 15) && (j_old > -15 && j_old < -5)) {
+			printf("попадание! x: %d, y: %d, шаг: %d\n", i_old, j_old, k);
+			break;
+		}
+		if (k == 50) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 1) {
+		printf("промах!\n");
+	}
+	return 0;
+}
